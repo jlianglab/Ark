74a9f32a2bc9d07f38fa9fbb40adb33ce5d9f3fb
Mda233 <dongaoma@asu.edu>
2023-10-07 02:18:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+<p align="center"><img width=15% alt="FrontCover" src="media/Ark_logo.png"></p>
+
+# Foundation Ark: Accruing and Reusing Knowledge
+
+We develop **open Foundation Models** from numerous public datasets using their heterogeneous expert annotations. Our Ark models outperform SOTA fully/self-supervised methods on various thoracic disease classification tasks and organ/bones segmentation tasks. Ark offers embeddings with superior quality over [Google's CXR Foundation Model](https://github.com/Google-Health/imaging-research/tree/master/cxr-foundation).
+
+
+
+
+
+## Publication
+<b>Foundation Ark: Accruing and Reusing Knowledge for Superior and Robust Performance </b> <br/>
+[DongAo Ma](https://github.com/Mda233)<sup>1</sup>, [Jiaxuan Pang](https://github.com/MRJasonP)<sup>1</sup>, [Michael B. Gotway](https://www.mayoclinic.org/biographies/gotway-michael-b-m-d/bio-20055566)<sup>2</sup>, [Jianming Liang](https://chs.asu.edu/jianming-liang)<sup>1</sup><br/>
+<sup>1 </sup>Arizona State University, <sup>2 </sup>Mayo Clinic <br/>
+
+International Conference on Medical Image Computing and Computer Assisted Intervention ([MICCAI 2023](https://conferences.miccai.org/2023/en/))
+
+[Paper](https://link.springer.com/chapter/10.1007/978-3-031-43907-0_62) | [Code](https://github.com/jlianglab/Ark) | [Poster](media/Ark_poster.pdf) | [Slides]() | Presentation ([YouTube]())
+
+
+### Requirements
++ Python
++ Install PyTorch ([pytorch.org](http://pytorch.org))
+
+
+## Pre-trained models
+
+You can download the pretrained models used/developed in our paper as follows:
+
+<table><tbody>
+<!-- START TABLE -->
+<!-- TABLE HEADER -->
+<th valign="bottom">Model</th>
+<th valign="bottom">Backbone</th>
+<th valign="bottom">Training Dataset</th>
+<th valign="bottom">model</th>
+
+<tr>
+<td>Ark-5</td>
+<td>Swin-Base</td>
+<td>ImageNet &#8594; X-rays(335K)</td>
+<td><a href="">Preparing</a></td>
+
+</tr>
+<tr> 
+<td>Ark-6</td>
+ <td>Swin-Base</td>
+<td>ImageNet &#8594; X-rays(704K)</td>
+
+<td><a href="">Preparing</a></td>
+ </tr>
+
+ 
+</tbody></table>
+
+
+
+## Citation
+If you use this code or use our pre-trained weights for your research, please cite our paper:
+```
+@InProceedings{ma2023foundation,
+    author="Ma, DongAo and Pang, Jiaxuan and Gotway, Michael B. and Liang, Jianming",
+    title="Foundation Ark: Accruing and Reusing Knowledge for Superior and Robust Performance",
+    booktitle="Medical Image Computing and Computer Assisted Intervention -- MICCAI 2023",
+    year="2023",
+    publisher="Springer Nature Switzerland",
+    address="Cham",
+    pages="651--662",
+    isbn="978-3-031-43907-0"
+}
+```
+
+## Acknowledgement
+This research has been supported in part by ASU and Mayo Clinic through a Seed Grant and an Innovation Grant, and in part by the NIH under Award Number R01HL128785. The content is solely the responsibility of the authors and does not necessarily represent the official views of the NIH. This work has utilized the GPUs provided in part by the ASU Research Computing and in part by the Bridges-2 at Pittsburgh Supercomputing Center through allocation BCS190015 and the Anvil at Purdue University through allocation MED220025 from the Advanced Cyberinfrastructure Coordination Ecosystem: Services & Support (ACCESS) program, which is supported by National Science Foundation grants #2138259, #2138286, #2138307, #2137603, and #2138296. We also acknowledge Google for granting us access to CXR Foundation API, which enabled us to generate the embeddings for the target datasets. The content of this paper is covered by patents pending.
+
+
+## License
+
+Released under the [ASU GitHub Project License](./LICENSE).# Ark
---
+++ utils.py
@@ -0,0 +1,146 @@
+from sklearn.metrics import roc_auc_score
+import torch
+import numpy as np
+import yaml
+from scipy import interpolate
+from PIL import Image
+
+def get_config(config):
+    with open(config, 'r') as stream:
+        return yaml.safe_load(stream)
+
+
+class MetricLogger(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressLogger(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def metric_AUROC(target, output, nb_classes=14):
+    outAUROC = []
+
+    target = target.cpu().numpy()
+    output = output.cpu().numpy()
+
+    for i in range(nb_classes):
+        outAUROC.append(roc_auc_score(target[:, i], output[:, i]))
+
+    return outAUROC
+
+
+def vararg_callback_bool(option, opt_str, value, parser):
+    assert value is None
+
+    arg = parser.rargs[0]
+    if arg.lower() in ('yes', 'true', 't', 'y', '1'):
+        value = True
+    elif arg.lower() in ('no', 'false', 'f', 'n', '0'):
+        value = False
+
+    del parser.rargs[:1]
+    setattr(parser.values, option.dest, value)
+
+
+def vararg_callback_int(option, opt_str, value, parser):
+    assert value is None
+    value = []
+
+    def intable(str):
+        try:
+            int(str)
+            return True
+        except ValueError:
+            return False
+
+    for arg in parser.rargs:
+        # stop on --foo like options
+        if arg[:2] == "--" and len(arg) > 2:
+            break
+        # stop on -a, but not on -3 or -3.0
+        if arg[:1] == "-" and len(arg) > 1 and not intable(arg):
+            break
+        value.append(int(arg))
+
+    del parser.rargs[:len(value)]
+    setattr(parser.values, option.dest, value)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def cosine_anneal_schedule(t,epochs,learning_rate):
+    T=epochs
+    M=1
+    alpha_zero = learning_rate
+
+    cos_inner = np.pi * (t % (T // M))  # t - 1 is used when t has 1-based indexing.
+    cos_inner /= T // M
+    cos_out = np.cos(cos_inner) + 1
+    return float(alpha_zero / 2 * cos_out)
+
+# --------------------------------------------------------
+# Copy from DINO https://github.com/facebookresearch/dino
+# Copyright (c) Facebook, Inc. and its affiliates.
+# --------------------------------------------------------
+def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epochs=0, start_warmup_value=0):
+    warmup_schedule = np.array([])
+    warmup_iters = warmup_epochs * niter_per_ep
+    if warmup_epochs > 0:
+        warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
+
+    iters = np.arange(epochs * niter_per_ep - warmup_iters)
+    schedule = final_value + 0.5 * (base_value - final_value) * (1 + np.cos(np.pi * iters / len(iters)))
+
+    schedule = np.concatenate((warmup_schedule, schedule))
+    assert len(schedule) == epochs * niter_per_ep
+    return schedule
